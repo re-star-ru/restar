3aec1f8478827740953183bcbff393c2bc4410a2
Ivan Cheremisin <fess932@gmail.com>
2022-08-19 16:02:54 +0300

    peer discovery

--- cmd/restar/main.go
+++ cmd/restar/main.go
@@ -1,19 +1,88 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/grandcat/zeroconf"
+	"log"
+	"math/rand"
 	"net/http"
-	"restar/pkg/user"
+	"time"
 )
 
 func main() {
-	println("Hello Restar")
+	log.Println("Hello Restar")
 
-	uc := user.NewUserUsecase()
-	uh := user.NewHttpHandler(uc)
+	id := uuid.NewString()
 
 	m := http.NewServeMux()
-	m.HandleFunc("/user/create", uh.Create)
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World, id: " + id))
+	})
 
-	println("listen at :9090")
-	http.ListenAndServe(":9090", m)
+	log.Println("id:", id)
+
+	port := openRandomPort()
+	host := fmt.Sprintf(":%d", port)
+
+	go registerPeer(id, port)
+	go listenForNewPeers(id)
+
+	log.Println("listen at:", host)
+	log.Println(http.ListenAndServe(host, m))
+
+	select {}
+}
+
+func registerPeer(id string, port int) {
+	server, err := zeroconf.Register(
+		id,
+		"_workstation._tcp",
+		"local.",
+		port,
+		[]string{"txtv=0", "lo=1", "la=2"},
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	defer server.Shutdown()
+
+	select {}
+}
+
+func listenForNewPeers(self string) {
+	resolver, err := zeroconf.NewResolver()
+	if err != nil {
+		log.Fatalf("Failed to initialize resolver: %s", err)
+	}
+
+	entries := make(chan *zeroconf.ServiceEntry)
+	go func(results <-chan *zeroconf.ServiceEntry) {
+		for entry := range results {
+			if entry.Instance == self {
+				continue
+			}
+
+			log.Printf("Found entry: id: %v, port: %v, ips: %v", entry.Instance, entry.Port, entry.AddrIPv4)
+		}
+	}(entries)
+
+	err = resolver.Browse(context.Background(), "_workstation._tcp", "local.", entries)
+	if err != nil {
+		log.Fatalln("Failed to browse:", err.Error())
+	}
+
+	select {}
+}
+
+func openRandomPort() int {
+	rand.Seed(time.Now().UnixNano())
+
+	start := 20000
+	end := 22000
+
+	return rand.Intn(end-start+1) + start //nolint:gosec
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module restar
 
 go 1.19
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/grandcat/zeroconf v1.0.0
+)
+
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/miekg/dns v1.1.27 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	golang.org/x/net v0.0.0-20220706163947-c90051bbdb60 // indirect
+	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
+)
