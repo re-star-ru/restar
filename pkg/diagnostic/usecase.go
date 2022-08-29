613a58fc08da2caac0e8c09a09293c54e5fc15e0
Ivan Cheremisin <fess932@gmail.com>
2022-08-29 10:44:12 +0300

    #3 добавляю акт дефектовки

--- pkg/diagnostic/usecase.go
+++ pkg/diagnostic/usecase.go
@@ -1,5 +1,10 @@
 package diagnostic
 
+import (
+	"context"
+	"restar/pkg/domain"
+)
+
 type Usecase struct {
 }
 
@@ -7,4 +12,18 @@ func NewUsecase() *Usecase {
 	return &Usecase{}
 }
 
-func (uc *Usecase) Create() {}
+func (uc *Usecase) Create(ctx context.Context) (domain.Diagnostic, error) {
+	return domain.Diagnostic{}, nil
+}
+
+func (uc *Usecase) Update(ctx context.Context, diag *domain.Diagnostic) error {
+	return nil
+}
+
+func (uc *Usecase) Read(ctx context.Context, id int64) (domain.Diagnostic, error) {
+	return domain.Diagnostic{}, nil
+}
+
+func (uc *Usecase) List(ctx context.Context) ([]domain.Diagnostic, error) {
+	return nil, nil
+}
